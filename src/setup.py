fe9ee82927e40f0f60e8af94cef076e7958bb7d6
Paolo Oliveri <paul@oliveri.info>
2022-07-18 08:00:32 +0000

    Patch for system wide installation and enabled GradsPy installation

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,5 @@
 from distutils.core import setup, Extension
+import numpy
+
 setup(name='gradspy', version='1.1',  \
-      ext_modules=[Extension('gradspy', ['gradspy.c'])])
+      ext_modules=[Extension('gradspy', ['gradspy.c'], include_dirs=[numpy.get_include()])])
